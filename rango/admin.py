2f43f99f68eba712f2d94d447372781a79157825
YIFENG SUN <2341623s@student.gla.ac.uk>
2018-01-27 17:44:22 +0000

    Chapter 5 update

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from rango.models import Category, Page, PageAdmin
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
+
+
